8ec530c6826a899c920d3a9d5a4cd5eafe83290b
tm_kentauros <tmkentauros@gmail.com>
2018-10-31 22:20:51 +0900

    End "3. templateを別ファイルに！".

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -3,12 +3,7 @@ import 'hero.dart';
 
 @Component(
   selector: 'my-app',
-  template: '''
-    <h1>{{title}}</h1>
-    <h2>{{hero.name}}</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div><label>name: </label>{{hero.name}}</div>
-  ''',
+  templateUrl: 'app_component.html',
 )
 class AppComponent {
   final title = 'Tour of Heroes';
