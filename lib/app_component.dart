8e447be7029a33ad9a144f04d85d9640188e835c
tm_kentauros <tmkentauros@gmail.com>
2018-11-01 07:20:54 +0900

    End "4. Heroを編集できるように！".

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,12 @@
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+
 import 'hero.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
+  directives: [formDirectives],
 )
 class AppComponent {
   final title = 'Tour of Heroes';
