53dc903a62b7990b769131f347e65cc5060bb980
tm_kentauros <tmkentauros@gmail.com>
2018-10-31 20:54:02 +0900

    End "2.Heroを表示！".

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,16 @@
 import 'package:angular/angular.dart';
+import 'hero.dart';
 
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  template: '''
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}}</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div><label>name: </label>{{hero.name}}</div>
+  ''',
 )
 class AppComponent {
-  var name = 'Angular';
+  final title = 'Tour of Heroes';
+  var hero = Hero(1, 'Windstorm');
 }
