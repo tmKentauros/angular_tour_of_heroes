168fc066247493a9135d8b55c2ce8e1fbf688813
tm_kentauros <tmkentauros@gmail.com>
2018-11-01 08:32:04 +0900

    End "6. Heroリストの見た目を変更！".

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -6,6 +6,7 @@ import 'src/mock_heroes.dart';
 
 @Component(
   selector: 'my-app',
+  styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
   directives: [coreDirectives, formDirectives],
 )
