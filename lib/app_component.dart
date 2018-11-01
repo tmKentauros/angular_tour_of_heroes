5725f2ef9cbcb029e81a5e7d270285cf0b5449cc
tm_kentauros <tmkentauros@gmail.com>
2018-11-01 15:04:23 +0900

    End "12. ボタンクリックでリストを表示！".

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,12 +1,14 @@
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
 
-import 'src/hero_list_component.dart';
+import 'src/routes.dart';
 
 @Component(
   selector: 'my-app',
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [HeroListComponent],
+  directives: [routerDirectives],
+  exports: [RoutePaths, Routes],
 )
 class AppComponent {
   final title = 'Tour of Heroes';
