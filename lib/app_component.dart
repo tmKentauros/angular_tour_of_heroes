80ceeb8212cbe84d49c698bba65439206db76f62
tm_kentauros <tmkentauros@gmail.com>
2018-11-01 10:07:27 +0900

    End "8. Heroをコンポーネント化！".

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,14 +1,15 @@
 import 'package:angular/angular.dart';
-import 'package:angular_forms/angular_forms.dart';
 
 import 'src/hero.dart';
 import 'src/mock_heroes.dart';
+
+import 'src/hero_component.dart';
 
 @Component(
   selector: 'my-app',
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [coreDirectives, formDirectives],
+  directives: [coreDirectives, HeroComponent],
 )
 class AppComponent {
   final title = 'Tour of Heroes';
---
+++ lib/src/hero_component.dart
@@ -0,0 +1,14 @@
+import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+
+import 'hero.dart';
+
+@Component(
+  selector: 'my-hero',
+  templateUrl: 'hero_component.html',
+  directives: [coreDirectives, formDirectives],
+)
+class HeroComponent {
+  @Input()
+  Hero hero;
+}
