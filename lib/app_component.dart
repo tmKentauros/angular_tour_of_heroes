afbe74eed4110a89ff67c20f0fda885d88bcf4f1
tm_kentauros <tmkentauros@gmail.com>
2018-11-01 10:31:29 +0900

    End "9. Heroをサービス化！".

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,20 +1,31 @@
 import 'package:angular/angular.dart';
 
 import 'src/hero.dart';
-import 'src/mock_heroes.dart';
 
 import 'src/hero_component.dart';
+import 'src/hero_service.dart';
 
 @Component(
   selector: 'my-app',
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
   directives: [coreDirectives, HeroComponent],
+  providers: [ClassProvider(HeroService)],
 )
-class AppComponent {
+class AppComponent implements OnInit {
   final title = 'Tour of Heroes';
-  List<Hero> heroes = mockHeroes;
+  List<Hero> heroes;
   Hero selected;
 
+  final HeroService _heroService;
+
+  AppComponent(this._heroService);
+
   void onSelect(Hero hero) => selected = hero;
+
+  void ngOnInit() => _getHeroes();
+
+  void _getHeroes() {
+    heroes = _heroService.getAll();
+  }
 }
