e1a224ed4bf358031a32bdb2f5d592cceca6c052
tm_kentauros <tmkentauros@gmail.com>
2018-11-02 10:56:28 +0900

    End "15. ダッシュボードを拡充！".

--- lib/src/dashboard_component.dart
+++ lib/src/dashboard_component.dart
@@ -1,7 +1,24 @@
 import 'package:angular/angular.dart';
 
+import 'hero.dart';
+import 'hero_service.dart';
+
 @Component(
   selector: 'my-dashboard',
   templateUrl: 'dashboard_component.html',
+  styleUrls: ['dashboard_component.css'],
+  directives: [coreDirectives],
+  providers: [ClassProvider(HeroService)],
 )
-class DashboardComponent {}
+class DashboardComponent implements OnInit {
+  List<Hero> heroes;
+
+  final HeroService _heroService;
+
+  DashboardComponent(this._heroService);
+
+  @override
+  void ngOnInit() async {
+    heroes = (await _heroService.getAll()).skip(1).take(4).toList();
+  }
+}
