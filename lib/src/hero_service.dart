c86d60ea41235029114ce9846209be2bab130e18
tm_kentauros <tmkentauros@gmail.com>
2018-11-02 18:33:34 +0900

    End "18. Heroのデータをサーバーから取得する！".

--- lib/src/hero_service.dart
+++ lib/src/hero_service.dart
@@ -1,10 +1,42 @@
-import 'hero.dart';
-import 'mock_heroes.dart';
-
 import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+import 'hero.dart';
 
 class HeroService {
-  Future<List<Hero>> getAll() async => mockHeroes;
-  Future<Hero> get(int id) async =>
-      (await getAll()).firstWhere((hero) => hero.id == id);
+  static const _heroesUrl = 'api/heroes';
+
+  final Client _http;
+
+  HeroService(this._http);
+
+  Future<List<Hero>> getAll() async {
+    try {
+      final response = await _http.get(_heroesUrl);
+      final heroes = (_extractData(response) as List)
+          .map((json) => Hero.fromJson(json))
+          .toList();
+      return heroes;
+    } catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  Future<Hero> get(int id) async {
+    try {
+      final response = await _http.get('$_heroesUrl/$id');
+      return Hero.fromJson(_extractData(response));
+    } catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  dynamic _extractData(Response resp) => json.decode(resp.body)['data'];
+
+  Exception _handleError(dynamic e) {
+    print(e); // for demo purposes only
+    return Exception('Server error; cause: $e');
+  }
 }
--- web/main.dart
+++ web/main.dart
@@ -1,12 +1,16 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
+import 'package:http/http.dart';
 
 import 'package:angular_app/app_component.template.dart' as ng;
+import 'package:angular_app/in_memory_data_service.dart';
+
 import 'main.template.dart' as self;
 
-@GenerateInjector(
+@GenerateInjector([
   routerProvidersHash,
-)
+  ClassProvider(Client, useClass: InMemoryDataService),
+])
 final InjectorFactory injector = self.injector$Injector;
 
 void main() {
